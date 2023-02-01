ebd25e59913f66252dc052d8c77151eff5fd916f
wjz2001 <wjz2001@users.noreply.github.com>
2023-02-01 17:26:20 +0800

    补充文件夹

--- js/search.js
+++
@@ -1,1 +0,0 @@
-function search(){if(document.getElementById("search_input").value != ""){window.location.href = "https://m.baidu.com/s?from=1011440l&word=" + document.getElementById("search_input").value;document.getElementById("search_input").value = "";}return false;}
