19c296db80a78adc7f2fd7895801c49e7c7d0be9
wjz2001 <wjz2001@users.noreply.github.com>
2023-02-08 00:32:16 +0800

    “优化代码”

--- js/search.js
+++ js/search.js
@@ -1,1 +1,6 @@
-function search(){if(document.getElementById("search_input").value != ""){window.location.href = "https://m.baidu.com/s?from=1011440l&word=" + document.getElementById("search_input").value;document.getElementById("search_input").value = "";}return false;}
+function search() {
+    if (document.querySelector(".search_bar input").value != "") {
+        location.href = "https://www.baidu.com/s?from=1011440l&word=" + document.querySelector(".search_bar input").value;
+        document.querySelector(".search_bar input").value = "";
+    } return false;
+}
