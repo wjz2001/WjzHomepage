32ec764dd4abedef403842e00423a51948392251
wjz2001 <wjz2001@users.noreply.github.com>
2023-02-15 16:18:47 +0800

    “更换加载动画，删除search.js，优化兼容性”

--- js/search.js
+++
@@ -1,6 +0,0 @@
-function search() {
-    if (document.querySelector(".search_bar input").value != "") {
-        location.href = "https://www.baidu.com/s?from=1011440l&word=" + document.querySelector(".search_bar input").value;
-        document.querySelector(".search_bar input").value = "";
-    } return false;
-}
