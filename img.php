24ced54031beefc52312963fd6f8080068361098
wjz2001 <wjz2001@users.noreply.github.com>
2023-03-14 21:57:45 +0800

    “顶图加载方式优化”

--- img.php
+++ img.php
@@ -1,6 +1,10 @@
 <?php
 $img_array = glob("img/background/*.{webp,jpg,png}",GLOB_BRACE); 
+$img_array_json = json_encode($img_array);
+echo "var img_array_json='$img_array_json';";
+/*
 $img = array_rand($img_array); 
 $dz = $img_array[$img];
 header("Location:".$dz);
+*/
 ?> 
